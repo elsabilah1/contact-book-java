bf3310b18ce1fe5f174b4a8dad3ba4f6c583d05a
elsabilah1 <elsabilah1@gmail.com>
2021-09-23 11:06:43 +0700

    membuat fitur menu, add data, dan show all data

--- src/ContactBook.java
+++ src/ContactBook.java
@@ -1,3 +1,72 @@
-public class ContactBook {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
+public class ContactBook {
+    static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+//        initialises array list contact data
+        ArrayList<ContactData> cbList = new ArrayList<>();
+
+        boolean flag = true;
+        while (flag){
+            System.out.println("\n==CONTACT BOOK==");
+            System.out.println("1. Add Data\n2. Delete Data\n3. Display Data \n4. Update Data\n5. Display All Data\n6. close app");
+            System.out.print("select menu: ");
+            int choice = in.nextInt();
+            switch (choice){
+                case 1:
+                    System.out.println("\nADDING DATA TO CONTACT BOOK");
+                    if (ContactData.getDataCount() <= ContactData.getMAXIMUMDATA()){
+                        System.out.print("Name\t\t: ");
+                        String name = in.next();
+                        System.out.print("Address\t\t: ");
+                        String address = in.next();
+                        in.nextLine();
+                        System.out.print("Phone Number\t: ");
+                        String phoneNum = in.next();
+                        System.out.print("Email\t\t: ");
+                        String email = in.next();
+                        cbList.add(new ContactData(name, address,phoneNum,email));
+                    } else{
+                        System.out.println("CB CAPACITY FULL, CAN'T ADD NEW DATA");
+                    }
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    System.out.println("\nCONTACT BOOK\nCapacity " + ContactData.getDataCount() + "/50");
+                    for(ContactData cd: cbList) {
+                        System.out.println(cd);
+                    }
+                    backToMenu();
+                    break;
+                case 6:
+                    flag = false;
+                    break;
+                default:
+                    System.out.println("error input");
+            }
+
+//            sorting data
+            Collections.sort(cbList, ContactData.nameComparator);
+        }
+    }
+
+    //    back to main menu option
+    public static void backToMenu(){
+        while (true) {
+            System.out.print("back to menu (y/n)?");
+            String cond = in.next();
+            if (cond.equalsIgnoreCase("y")){
+                break;
+            }
+        }
+    }
 }
--- src/ContactData.java
+++ src/ContactData.java
@@ -1,16 +1,27 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Scanner;
+
 public class ContactData {
+    private static int MAXIMUMDATA = 50;
+    private static int dataCount;
     private String name;
     private String address;
     private String phoneNumber;
     private String email;
-    private static int dataCount;
 
 //    CONSTRUCTOR
-    public ContactData (){
+
+
+    public ContactData(String name, String address, String phoneNumber, String email) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
         dataCount++;
     }
 
-//    ACCESSOR AND MUTATOR METHODS
+    //    ACCESSOR AND MUTATOR METHODS
     public String getName() {
         return name;
     }
@@ -51,5 +62,34 @@ public class ContactData {
         ContactData.dataCount = dataCount;
     }
 
-//    MORE METHODS
+    public static int getMAXIMUMDATA() {
+        return MAXIMUMDATA;
+    }
+
+    public static void setMAXIMUMDATA(int MAXIMUMDATA) {
+        ContactData.MAXIMUMDATA = MAXIMUMDATA;
+    }
+
+    //    MORE METHODS
+//    Sorting list by contact name
+    public static Comparator<ContactData> nameComparator = new Comparator<ContactData>() {
+    @Override
+    public int compare(ContactData c1, ContactData c2) {
+        String contactName1 = c1.getName().toUpperCase();
+        String contactName2 = c2.getName().toUpperCase();
+
+//        ascending order
+        return contactName1.compareTo(contactName2);
+    }};
+
+//    display contact data
+    @Override
+    public String toString() {
+        return "{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
